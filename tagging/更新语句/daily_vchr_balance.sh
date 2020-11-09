df829826ed46b321d4070daf46d76f8e047581b6
myc <770254505@qq.com>
2020-11-09 14:47:39 +0800

    update

--- tagging/更新语句/daily_vchr_balance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-td=`date +%Y-%m-%d`
-/usr/local/spark-2.2.0-bin-2.6.0-cdh5.8.0/bin/spark-sql --master yarn --deploy-mode client --driver-memory 2g --num-executors 4 --executor-cores 4 --executor-memory 16g  -e"
-insert overwrite table u_analysis_app.member_profile_vchr_balance_daily partition(pdate='${td}',type ='core')
-select member_id,count(*) core_balance from u_analysis_dw.core_skrit_vchr where type='core' and to_date(expiration_dt)>=current_date() and status_cd='Available'   group by member_id;
-insert overwrite table u_analysis_app.member_profile_vchr_balance_daily partition(pdate='${td}',type ='srkit')
-select member_id,count(*) core_balance from u_analysis_dw.core_skrit_vchr where type='srkit' and to_date(expiration_dt)>=current_date() and status_cd='Available'   group by member_id;"
--- tagging/更新语句/update_demo_facts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-td=`date +%Y-%m-%d`
-/usr/local/spark-2.2.0-bin-2.6.0-cdh5.8.0/bin/spark-sql --master yarn --deploy-mode client --driver-memory 1g --num-executors 4 --executor-cores 3 --executor-memory 15g -e "
-use u_analysis_app;
-insert overwrite table u_analysis_app.member_profile_Demo_facts partition(pdate='${td}') 
-select  member_id,age,birth_dt,case sex_mf when 'Female' then 1 when 'Male' then 0 else null end,city_tier,homecity,reg_channel_cd,submit_Dt  from u_analysis_dw.siebel_member;"
